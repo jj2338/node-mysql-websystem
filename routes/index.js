584bbbc8603fec641a5dc7118a5c5a5ddbec7760
jj2338 <j2338@matsue-ct.ac.jp>
2026-06-25 14:27:51 +0000

    サインイン機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const knex = require('../db/knex');
 const mysql = require('mysql');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  console.log(`isAuth: ${isAuth}`);
+
+
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
@@ -37,5 +41,6 @@ router.post('/', function (req, res, next) {
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
